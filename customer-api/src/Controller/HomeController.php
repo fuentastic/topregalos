7485bfa29bd14c0796d58126d4763de159d8240a
Miguel Fuentes <miguel@midgetgiraffe.com>
2024-04-15 16:30:56 +0200

    added `HomeController` + 'PhPUnit` -> `HomeControllerTest`

---
+++ customer-api/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return new Response('Hello Topgeschenken!');
+    }
+}
+
